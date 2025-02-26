91d61bacff9ad2399054550f93438a8b29fbcad7
Ian Lambert <ian.lambert@kellogg.northwestern.edu>
2025-02-26 06:44:05 +0000

    set up the post beginning

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,13 +1,27 @@
 class EntriesController < ApplicationController
 
   def index
-    
+    # find all Post rows
+    @posts = Entry.all
+    # render posts/index view
+  end
 
-    # find all places rows
-    
+  def new
+    # render posts/new view with new Post form
+  end
 
+  def create
+    # start with a new Post
+    @post = Entry.new
+    # assign user-entered form data to Post's columns
+    @post["title"] = params["title"]
+    @post["description"] = params["description"]
+    @post["date"] = params["date"]
+    # save Post row
+    @post.save
+    # redirect user
+    redirect_to "/entries"
+  end
 
-    render :template => "entries/index"
-  end 
 
 end
